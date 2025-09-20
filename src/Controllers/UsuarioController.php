bf25c6451fa6c2e82f249d308cb5710f5c78d439
Joao Paulo S Paiva <bypaiva@gmail.com>
2025-09-20 14:16:13 -0300

    busca usuario

--- src/Controllers/UsuarioController.php
+++ src/Controllers/UsuarioController.php
@@ -31,11 +31,46 @@ final class UsuarioController {
     }
 
 
-    public function busca(Request $request, Response $response): Response
+    public function atualiza(Request $request, Response $response): Response
     {
+        $data = $request->getParsedBody();
+        $nome = $data['nome'] ?? '';
+        $email = $data['email'] ?? '';
+        $senha = $data['senha'] ?? '';
+        $codusuario = $data['codusuario'] ?? '';
+
+        if (empty($codusuario) || empty($nome) || empty($email) || empty($senha)) {
+            $response->getBody()->write(json_encode(
+                [
+                    'error' => true,
+                    'message' => 'Todos os campos são obrigatórios',
+                    'data' => $data
+                ]
+            ));
+            return $response;
+        }
+
         $usuarioDAO = new UsuarioDAO();
-        $result = $usuarioDAO->getUSuarios();
+        $result = $usuarioDAO->atualizaUsuario($data);
         $response->getBody()->write(json_encode($result));
+
         return $response;
     }
+
+    public function busca(Request $request, Response $response): Response {
+        $usuarioDAO = new UsuarioDAO();
+        $result = $usuarioDAO->getUSuarios();
+        $response->getBody()->write(json_encode(
+            [
+                'error' => false,
+                'message' => count($result) > 0 ? 'Registros encontrados': 'Nenhum registro encontrado',
+                'data' => $result
+            ]
+        ));
+        return $response;
+    }
+
+    public function excluir(Request $request, Response $response): Response {
+        return $response;
+    }   
 }
--- src/DAO/UsuarioDAO.php
+++ src/DAO/UsuarioDAO.php
@@ -3,6 +3,7 @@
 namespace App\DAO;
 
 use PDO;
+use PDOException;
 
 final class UsuarioDAO extends Conexao {
 
@@ -34,10 +35,72 @@ final class UsuarioDAO extends Conexao {
         return $result;
     }
 
-    public function getUsuarios() {
-        $sql = "SELECT * FROM usuario";
+    public function atualizaUsuario($data) {
+        $sql = 'UPDATE USUARIO SET NOME = :NOME, EMAIL = :EMAIL, SENHA = :SENHA WHERE CODUSUARIO = :CODUSUARIO';
         $stmt = $this->pdo->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll();
+        $stmt->bindValue(':NOME', $data['nome'], PDO::PARAM_STR);
+        $stmt->bindValue(':EMAIL', $data['email'], PDO::PARAM_STR);
+        $stmt->bindValue(':SENHA', md5($data['senha']), PDO::PARAM_STR);
+        $stmt->bindValue(':CODUSUARIO', $data['codusuario'], PDO::PARAM_INT);
+        if ($stmt->execute()) {
+            $result = array(
+                'error' => false, 
+                'message' => 'Usuário atualizado com sucesso', 
+                'data' => []
+            );
+        } else {
+            $result = array(
+                'error' => true, 
+                'message' => 'Erro ao atualizar usuário', 
+                'data' => []
+            );
+        }
+        
+        return $result;
+    }
+
+    public function getUsuarios() {
+        try {
+            $sql = "SELECT CODUSUARIO, NOME, EMAIL FROM usuario";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute();
+            
+            return $stmt->fetchAll(PDO::FETCH_ASSOC) ?: [];
+        } catch (PDOException $e) {
+            // Logar ou tratar erro
+            return [];
+        }
+    }
+
+    public function getUsuarioByEmail($email) {
+        try {
+            $sql = "SELECT CODUSUARIO, NOME, EMAIL, SENHA
+            FROM usuario
+            WHERE EMAIL = :EMAIL";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindValue(':EMAIL', $email, PDO::PARAM_STR);
+            $stmt->execute();
+            
+            return $stmt->fetch(PDO::FETCH_ASSOC) ?: [];
+        } catch (PDOException $e) {
+            // Logar ou tratar erro
+            return [];
+        }
+    }
+
+    public function getUsuarioByCodUsuario($codusuario) {
+        try {
+            $sql = "SELECT CODUSUARIO, NOME, EMAIL, SENHA
+            FROM usuario
+            WHERE EMAIL = :CODUSUARIO";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->bindValue(':CODUSUARIO', $codusuario, PDO::PARAM_INT);
+            $stmt->execute();
+            
+            return $stmt->fetch(PDO::FETCH_ASSOC) ?: [];
+        } catch (PDOException $e) {
+            // Logar ou tratar erro
+            return [];
+        }
     }
 } 
